42fe6b955a2909a5e430f80955899e10d6885866
junyeob.kim <wnsduq8282@gmail.com>
2025-02-09 22:46:53 +0900

    rendering that item

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 // Project Type
-
 enum ProjectStatus {
     Active,
     Finished,
@@ -15,6 +14,7 @@ class Project {
     ) {}
 }
 
+// Project State Management
 type Listener<T> = (items: T[]) => void;
 
 class State<T> {
@@ -25,7 +25,6 @@ class State<T> {
     }
 }
 
-// Project State Management
 class ProjectState extends State<Project> {
     private projects: Project[] = [];
     private static instance: ProjectState;
@@ -38,7 +37,6 @@ class ProjectState extends State<Project> {
         if (this.instance) {
             return this.instance;
         }
-
         this.instance = new ProjectState();
         return this.instance;
     }
@@ -51,11 +49,7 @@ class ProjectState extends State<Project> {
             numOfPeople,
             ProjectStatus.Active
         );
-
         this.projects.push(newProject);
-
-        // addProject가 발생할때 모든 listener 함수를 호출한다.
-        // 이게 구독 패턴.
         for (const listenerFn of this.listeners) {
             listenerFn(this.projects.slice());
         }
@@ -64,7 +58,7 @@ class ProjectState extends State<Project> {
 
 const projectState = ProjectState.getInstance();
 
-// Validatoion
+// Validation
 interface Validatable {
     value: string | number;
     required?: boolean;
@@ -80,39 +74,34 @@ function validate(validatableInput: Validatable) {
         isValid =
             isValid && validatableInput.value.toString().trim().length !== 0;
     }
-
     if (
         validatableInput.minLength != null &&
         typeof validatableInput.value === "string"
     ) {
         isValid =
             isValid &&
-            validatableInput.value.trim().length >= validatableInput.minLength;
+            validatableInput.value.length >= validatableInput.minLength;
     }
-
     if (
         validatableInput.maxLength != null &&
         typeof validatableInput.value === "string"
     ) {
         isValid =
             isValid &&
-            validatableInput.value.trim().length <= validatableInput.maxLength;
+            validatableInput.value.length <= validatableInput.maxLength;
     }
-
     if (
         validatableInput.min != null &&
         typeof validatableInput.value === "number"
     ) {
         isValid = isValid && validatableInput.value >= validatableInput.min;
     }
-
     if (
         validatableInput.max != null &&
         typeof validatableInput.value === "number"
     ) {
         isValid = isValid && validatableInput.value <= validatableInput.max;
     }
-
     return isValid;
 }
 
@@ -126,9 +115,9 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
             return boundFn;
         },
     };
-
     return adjDescriptor;
 }
+
 // Component Base Class
 abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     templateElement: HTMLTemplateElement;
@@ -151,7 +140,9 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
             true
         );
         this.element = importedNode.firstElementChild as U;
-        if (newElementId) this.element.id = newElementId;
+        if (newElementId) {
+            this.element.id = newElementId;
+        }
 
         this.attach(insertAtStart);
     }
@@ -168,51 +159,24 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 // ProjectItem Class
-// 여기 있는 것을 구현하자
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
-    private singleProject: Project;
-    private appendElement: HTMLUListElement;
-    // private li
+    private project: Project;
 
-    constructor(project: Project, appendElement: HTMLUListElement) {
-        super("single-project", "app", false, "render-list");
-        this.singleProject = project;
-        this.appendElement = appendElement;
+    constructor(hostId: string, project: Project) {
+        super("single-project", hostId, false, project.id);
+        this.project = project;
+
+        this.configure();
+        this.renderContent();
     }
+
+    configure() {}
 
     renderContent() {
-        const titleElement = this.element.querySelector("h2");
-        if (titleElement) titleElement.innerText = this.singleProject.title!;
-
-        const descElement = this.element.querySelector("h3");
-        if (descElement) descElement.innerText = this.singleProject.description;
-
-        const peopleElement = this.element.querySelector("p");
-        if (peopleElement)
-            peopleElement.innerText = String(this.singleProject.people);
-
-        this.appendElement.append(this.element);
-    }
-    configure() {}
-}
-
-class ElementRender {
-    listEl: HTMLUListElement;
-
-    constructor(elementId: string) {
-        this.listEl = document.getElementById(elementId)! as HTMLUListElement;
-        this.clearElement();
-    }
-
-    renderElement(assignedProjects: Project[]) {
-        for (const prjItem of assignedProjects) {
-            const renderProjectInner = new ProjectItem(prjItem, this.listEl);
-            renderProjectInner.renderContent();
-        }
-    }
-
-    private clearElement() {
-        this.listEl.innerHTML = "";
+        this.element.querySelector("h2")!.textContent = this.project.title;
+        this.element.querySelector("h3")!.textContent =
+            this.project.people.toString();
+        this.element.querySelector("p")!.textContent = this.project.description;
     }
 }
 
@@ -224,35 +188,21 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
         super("project-list", "app", false, `${type}-projects`);
         this.assignedProjects = [];
 
-        // 뭔가가 변경이 된다면 해당 함수가 호출되어야한다?
-        // listener로 등록한 함수들이 모두 호출되게 된다. state의 projects가 매개변수로 들어간다.
-        // 따라서 assignedProject는 state의 project로 갱신되게 되고,
-        // 갱신된 정보를 가지고 rendering을 하게된다.
-
         this.configure();
         this.renderContent();
     }
 
     configure() {
         projectState.addListener((projects: Project[]) => {
-            // 여기서 렌더링되는 프로젝트는 all이다.
-            // 따라서, filter를 걸어줘야한다.
             const relevantProjects = projects.filter((prj) => {
                 if (this.type === "active") {
                     return prj.status === ProjectStatus.Active;
-                } else {
-                    return prj.status === ProjectStatus.Finished;
                 }
+                return prj.status === ProjectStatus.Finished;
             });
             this.assignedProjects = relevantProjects;
             this.renderProjects();
         });
-    }
-
-    // add project 버튼을 눌렀을 때 rendering 되는 함수.
-    private renderProjects() {
-        const elementRender = new ElementRender(`${this.type}-projects-list`);
-        elementRender.renderElement(this.assignedProjects);
     }
 
     renderContent() {
@@ -261,9 +211,19 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
         this.element.querySelector("h2")!.textContent =
             this.type.toUpperCase() + " PROJECTS";
     }
+
+    private renderProjects() {
+        const listEl = document.getElementById(
+            `${this.type}-projects-list`
+        )! as HTMLUListElement;
+        listEl.innerHTML = "";
+        for (const prjItem of this.assignedProjects) {
+            new ProjectItem(this.element.querySelector("ul")!.id, prjItem);
+        }
+    }
 }
 
-// ProjectInput class
+// ProjectInput Class
 class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     titleInputElement: HTMLInputElement;
     descriptionInputElement: HTMLInputElement;
@@ -271,26 +231,23 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
 
     constructor() {
         super("project-input", "app", true, "user-input");
-
         this.titleInputElement = this.element.querySelector(
             "#title"
-        )! as HTMLInputElement;
+        ) as HTMLInputElement;
         this.descriptionInputElement = this.element.querySelector(
             "#description"
-        )! as HTMLInputElement;
+        ) as HTMLInputElement;
         this.peopleInputElement = this.element.querySelector(
             "#people"
-        )! as HTMLInputElement;
-
+        ) as HTMLInputElement;
         this.configure();
     }
 
-    renderContent() {}
-
     configure() {
-        // event lisnter
         this.element.addEventListener("submit", this.submitHandler);
     }
+
+    renderContent() {}
 
     private gatherUserInput(): [string, string, number] | void {
         const enteredTitle = this.titleInputElement.value;
@@ -301,13 +258,11 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
             value: enteredTitle,
             required: true,
         };
-
         const descriptionValidatable: Validatable = {
             value: enteredDescription,
             required: true,
             minLength: 5,
         };
-
         const peopleValidatable: Validatable = {
             value: +enteredPeople,
             required: true,
@@ -320,7 +275,7 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
             !validate(descriptionValidatable) ||
             !validate(peopleValidatable)
         ) {
-            alert("invalid input. please try again");
+            alert("Invalid input, please try again!");
             return;
         } else {
             return [enteredTitle, enteredDescription, +enteredPeople];
@@ -338,13 +293,13 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
         event.preventDefault();
         const userInput = this.gatherUserInput();
         if (Array.isArray(userInput)) {
-            const [title, description, people] = userInput;
-            projectState.addProject(title, description, people);
+            const [title, desc, people] = userInput;
+            projectState.addProject(title, desc, people);
             this.clearInputs();
         }
     }
 }
 
 const prjInput = new ProjectInput();
-const activeProjectList = new ProjectList("active");
-const finisehdProjectList = new ProjectList("finished");
+const activePrjList = new ProjectList("active");
+const finishedPrjList = new ProjectList("finished");
