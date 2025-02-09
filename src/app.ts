6ac7ff58a21fd40e2e7b395981b6c3fe4b4d0472
junyeob.kim <wnsduq8282@gmail.com>
2025-02-09 23:02:43 +0900

    initialize drag&drop

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+// Drag & Drop Interfaces
+interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {
+    dragOverHandler(event: DragEvent): void;
+    dropHandler(event: DragEvent): void;
+    dragLeaveHandler(event: DragEvent): void;
+}
+
 // Project Type
 enum ProjectStatus {
     Active,
@@ -159,8 +171,19 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 // ProjectItem Class
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+class ProjectItem
+    extends Component<HTMLUListElement, HTMLLIElement>
+    implements Draggable
+{
     private project: Project;
+
+    get peresons() {
+        if (this.project.people === 1) {
+            return "1 person";
+        } else {
+            return `${this.project.people} persons`;
+        }
+    }
 
     constructor(hostId: string, project: Project) {
         super("single-project", hostId, false, project.id);
@@ -169,13 +192,24 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
         this.configure();
         this.renderContent();
     }
+    @autobind
+    dragStartHandler(event: DragEvent) {
+        console.log(event);
+    }
 
-    configure() {}
+    dragEndHandler(event: DragEvent) {
+        console.log("Drag End");
+    }
+
+    configure() {
+        this.element.addEventListener("dragstart", this.dragStartHandler);
+        this.element.addEventListener("dragend", this.dragEndHandler);
+    }
 
     renderContent() {
         this.element.querySelector("h2")!.textContent = this.project.title;
         this.element.querySelector("h3")!.textContent =
-            this.project.people.toString();
+            this.peresons.toString() + " assigned";
         this.element.querySelector("p")!.textContent = this.project.description;
     }
 }
