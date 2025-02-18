5f8b691ef69e6044e75c43f47fe3dde4831fa613
junyeob.kim <wnsduq8282@gmail.com>
2025-02-18 23:56:20 +0900

    Use class-transformer

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,25 @@
 import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
-import _ from "lodash";
+import { Product } from "./product.model";
+import "reflect-metadata";
+// import _ from "lodash";
+import { plainToClass } from "class-transformer";
 
 new ProjectInput();
 new ProjectList("active");
 new ProjectList("finished");
+
+const products = [
+    { title: "A Carpet", price: 29.99 },
+    { title: "a book", price: 10.99 },
+];
+
+// const loadedProducts = products.map((prod) => {
+//     return new Product(prod.title, prod.price);
+// });
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+    console.log(prod.getInformation());
+}
